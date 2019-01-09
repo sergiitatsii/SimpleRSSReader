45444899e56f96431032e6cf41961f9a77f2ae6d
Sergii Tatsii <sergiitatsii@gmail.com>
2019-01-09 13:26:39 +0300

    Initial Commit

---
+++ SimpleRSSReaderTests/SimpleRSSReaderTests.swift
@@ -0,0 +1,34 @@
+//
+//  SimpleRSSReaderTests.swift
+//  SimpleRSSReaderTests
+//
+//  Created by Sergii Tatsii on 1/9/19.
+//  Copyright © 2019 Sergii Tatsii. All rights reserved.
+//
+
+import XCTest
+@testable import SimpleRSSReader
+
+class SimpleRSSReaderTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
